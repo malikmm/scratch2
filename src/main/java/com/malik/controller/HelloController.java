7511b929b0bb62a4fc9b0cce4f8ba22c825b79df
Mohammed Malik <malikmm@hotmail.com>
2023-02-07 15:52:34 +0000

    warnings removed

--- src/main/java/com/malik/controller/HelloController.java
+++ src/main/java/com/malik/controller/HelloController.java
@@ -8,7 +8,7 @@ package com.malik.controller;
  *
  */
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
