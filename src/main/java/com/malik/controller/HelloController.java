5e43dc90b81d2fd3548ff5a65f2dcb7aae14baed
Mohammed Malik <malikmm@hotmail.com>
2023-02-08 17:08:33 +0000

    org.slf4j logger added

--- src/main/java/com/malik/controller/HelloController.java
+++ src/main/java/com/malik/controller/HelloController.java
@@ -3,20 +3,18 @@
  */
 package com.malik.controller;
 
-/**
- * @author malikmo
- *
- */
-
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-//	@GetMapping("/")
+	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+	
 	@RequestMapping("/")
 	public String index() {
+		log.info("Greetings from Sping Boot");
 		return "Greetings from Sping Boot";
 	}
 	
